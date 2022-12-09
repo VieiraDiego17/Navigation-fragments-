4bdbd04fd9b725f44b4c98761b204940ca00fbe0
Diego Vieira <diegovieira@C02CTJ5UMD6M.local>
2022-12-09 14:53:53 -0300

    5 telas feitas

--- app/src/main/java/com/example/navigation/FragmentFirst.kt
+++ app/src/main/java/com/example/navigation/FragmentFirst.kt
@@ -3,7 +3,9 @@ package com.example.navigation
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 
 class FragmentFirst : Fragment(R.layout.fragment_first) {
@@ -24,6 +26,13 @@ class FragmentFirst : Fragment(R.layout.fragment_first) {
             findNavController().navigate(R.id.actionFirstToFifth)
         }
 
-
+//        val mensagemRecebida = view?.findViewById<EditText>(R.id.numeroDigitado)
+//        val mensagemConvertida = mensagemRecebida?.text.toString().toInt()
+//        val acao = FragmentFirstDirections.actionFirstToSecond(
+//            number = mensagemConvertida
+//        )
+//        view.findViewById<Button>(R.id.buttonEnviarInfo).setOnClickListener {
+//            findNavController().navigate(acao)
+//        }
     }
 }
--- app/src/main/java/com/example/navigation/FragmentSecond.kt
+++ app/src/main/java/com/example/navigation/FragmentSecond.kt
@@ -4,11 +4,16 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+
 
 class FragmentSecond : Fragment(R.layout.fragment_second) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
 
         view.findViewById<Button>(R.id.buttonSecondToFirst).setOnClickListener {
             findNavController().navigate(R.id.actionSecondToFirst)
@@ -23,5 +28,11 @@ class FragmentSecond : Fragment(R.layout.fragment_second) {
             findNavController().navigate(R.id.actionSecondToFifth)
         }
 
+//        val args: FragmentSecondArgs by navArgs()
+//
+//        val mensagemRecebida = view.findViewById<TextView>(R.id.textMensagem)
+//        val number = args.number
+//        //mensagemRecebida.text = number.toString()
+//        mensagemRecebida.text = "${args.number}"
     }
     }
