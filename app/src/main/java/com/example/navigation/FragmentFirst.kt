f236f1d33986d4fd7d03e57fbe6de59960573113
Diego Vieira <diegovieira.ads@gmail.com>
2022-12-10 15:42:54 -0300

    Alteração de identação dos códigos

--- app/src/main/java/com/example/navigation/FragmentFirst.kt
+++ app/src/main/java/com/example/navigation/FragmentFirst.kt
@@ -13,9 +13,32 @@ class FragmentFirst : Fragment(R.layout.fragment_first) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        navigation(view)
+
+        sendInfo(view)
+
+    }
+
+    fun sendInfo(view: View) {
+        val info = view?.findViewById<EditText>(R.id.numeroDigitado)
+        val infoToInt = info?.text.toString().toInt()
+
         view.findViewById<Button>(R.id.buttonFirstToSecond).setOnClickListener {
-            findNavController().navigate(R.id.actionFirstToSecond)
+            val direction = FragmentFirstDirections.actionFirstToSecond(number = infoToInt)
+            findNavController().navigate(direction)
         }
+
+//        view.findViewById<Button>(R.id.buttonEnviarInfo).setOnClickListener {
+//            val direction = FragmentFirstDirections.actionFirstToSecond()
+//            direction.number = infoToInt
+//            findNavController().navigate(direction)
+//        }
+    }
+
+    fun navigation(view: View) {
+//        view.findViewById<Button>(R.id.buttonFirstToSecond).setOnClickListener {
+//            findNavController().navigate(R.id.actionFirstToSecond)
+//        }
         view.findViewById<Button>(R.id.buttonFirstToThird).setOnClickListener {
             findNavController().navigate(R.id.actionFirstToThird)
         }
@@ -25,14 +48,5 @@ class FragmentFirst : Fragment(R.layout.fragment_first) {
         view.findViewById<Button>(R.id.buttonFirstToFifth).setOnClickListener {
             findNavController().navigate(R.id.actionFirstToFifth)
         }
-
-//        val mensagemRecebida = view?.findViewById<EditText>(R.id.numeroDigitado)
-//        val mensagemConvertida = mensagemRecebida?.text.toString().toInt()
-//        val acao = FragmentFirstDirections.actionFirstToSecond(
-//            number = mensagemConvertida
-//        )
-//        view.findViewById<Button>(R.id.buttonEnviarInfo).setOnClickListener {
-//            findNavController().navigate(acao)
-//        }
     }
 }
--- app/src/main/java/com/example/navigation/FragmentSecond.kt
+++ app/src/main/java/com/example/navigation/FragmentSecond.kt
@@ -5,16 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 
 
 class FragmentSecond : Fragment(R.layout.fragment_second) {
+
+    val argumentos: FragmentSecondArgs by navArgs()
+    //val argumentos by navArgs<FragmentSecondArgs>()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        navigation(view)
 
+        val info = argumentos.number
+        //val infoReceived = view.findViewById<TextView>(R.id.textMensagem).setText(info)
+        val infoReceived = view.findViewById<TextView>(R.id.textMensagem)
+        //infoReceived.text = info.toString()
+        infoReceived.text = "${info}"
+//        val mensagemRecebida = view.findViewById<TextView>(R.id.textMensagem)
+//        val number = args.number
+//        //mensagemRecebida.text = number.toString()
+//        mensagemRecebida.text = "${args.number}"
+    }
 
+    fun navigation(view: View){
         view.findViewById<Button>(R.id.buttonSecondToFirst).setOnClickListener {
             findNavController().navigate(R.id.actionSecondToFirst)
         }
@@ -27,12 +44,5 @@ class FragmentSecond : Fragment(R.layout.fragment_second) {
         view.findViewById<Button>(R.id.buttonSecondToFifth).setOnClickListener {
             findNavController().navigate(R.id.actionSecondToFifth)
         }
-
-//        val args: FragmentSecondArgs by navArgs()
-//
-//        val mensagemRecebida = view.findViewById<TextView>(R.id.textMensagem)
-//        val number = args.number
-//        //mensagemRecebida.text = number.toString()
-//        mensagemRecebida.text = "${args.number}"
     }
     }
